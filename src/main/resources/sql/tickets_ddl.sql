3e78b25b9aebfe33644143b3d86d7316a161fc0c
Vlad Pop <vlad4800@gmail.com>
2017-01-20 20:08:31 +0200

    sql update

--- src/main/resources/sql/tickets_ddl.sql
+++ src/main/resources/sql/tickets_ddl.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: Jan 19, 2017 at 02:27 PM
+-- Generation Time: Jan 20, 2017 at 06:06 PM
 -- Server version: 10.1.20-MariaDB
 -- PHP Version: 7.0.14
 
@@ -48,6 +48,7 @@ DROP TABLE IF EXISTS `tickets`;
 CREATE TABLE IF NOT EXISTS `tickets` (
 `id` int(10) unsigned NOT NULL,
   `ticket_county_id` smallint(6) NOT NULL,
+  `user_id` int(10) unsigned DEFAULT NULL,
   `type` enum('furt','crima','viol','generic') NOT NULL DEFAULT 'generic',
   `device_id` varchar(100) NOT NULL,
   `ip` varchar(50) NOT NULL,
@@ -80,8 +81,7 @@ CREATE TABLE IF NOT EXISTS `ticket_attachments` (
   `create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `update_date` datetime DEFAULT NULL,
   `delete_date` datetime DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
-
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 -- --------------------------------------------------------
 
 --
@@ -95,6 +95,48 @@ CREATE TABLE IF NOT EXISTS `ticket_messages` (
   `name` varchar(200) DEFAULT NULL,
   `subject` varchar(255) NOT NULL,
   `message` text NOT NULL,
+  `create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_date` datetime DEFAULT NULL,
+  `delete_date` datetime DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tokens`
+--
+
+DROP TABLE IF EXISTS `tokens`;
+CREATE TABLE IF NOT EXISTS `tokens` (
+`id` int(10) unsigned NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  `type` enum('bearer','device') DEFAULT 'bearer',
+  `value` char(40) NOT NULL,
+  `expiry_date` datetime DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `update_date` datetime DEFAULT NULL,
+  `delete_date` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+`id` int(10) unsigned NOT NULL,
+  `role` enum('admin','user') NOT NULL DEFAULT 'user',
+  `device_id` varchar(100) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(103) NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `county_id` smallint(6) NOT NULL,
+  `cnp` varchar(13) NOT NULL,
+  `phone` varchar(50) NOT NULL,
+  `status` enum('active','inactive') NOT NULL DEFAULT 'inactive',
   `create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `update_date` datetime DEFAULT NULL,
   `delete_date` datetime DEFAULT NULL
@@ -114,7 +156,7 @@ ALTER TABLE `counties`
 -- Indexes for table `tickets`
 --
 ALTER TABLE `tickets`
- ADD PRIMARY KEY (`id`), ADD KEY `county_id` (`county_id`), ADD KEY `ticket_county_id` (`ticket_county_id`);
+ ADD PRIMARY KEY (`id`), ADD KEY `county_id` (`county_id`), ADD KEY `ticket_county_id` (`ticket_county_id`), ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `ticket_attachments`
@@ -129,6 +171,18 @@ ALTER TABLE `ticket_messages`
  ADD PRIMARY KEY (`id`), ADD KEY `ticket_id` (`ticket_id`);
 
 --
+-- Indexes for table `tokens`
+--
+ALTER TABLE `tokens`
+ ADD PRIMARY KEY (`id`), ADD KEY `expiry_date` (`expiry_date`), ADD KEY `type` (`type`,`value`), ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `cnp` (`cnp`), ADD UNIQUE KEY `email` (`email`), ADD KEY `county_id` (`county_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -141,11 +195,21 @@ MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 -- AUTO_INCREMENT for table `ticket_attachments`
 --
 ALTER TABLE `ticket_attachments`
-MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `ticket_messages`
 --
 ALTER TABLE `ticket_messages`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `tokens`
+--
+ALTER TABLE `tokens`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
 MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- Constraints for dumped tables
@@ -156,7 +220,8 @@ MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 ALTER TABLE `tickets`
 ADD CONSTRAINT `tickets_ibfk_1` FOREIGN KEY (`county_id`) REFERENCES `counties` (`id`) ON UPDATE CASCADE,
-ADD CONSTRAINT `tickets_ibfk_2` FOREIGN KEY (`ticket_county_id`) REFERENCES `counties` (`id`) ON UPDATE CASCADE;
+ADD CONSTRAINT `tickets_ibfk_2` FOREIGN KEY (`ticket_county_id`) REFERENCES `counties` (`id`) ON UPDATE CASCADE,
+ADD CONSTRAINT `tickets_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `ticket_attachments`
@@ -169,6 +234,12 @@ ADD CONSTRAINT `ticket_attachments_ibfk_1` FOREIGN KEY (`ticket_id`) REFERENCES 
 --
 ALTER TABLE `ticket_messages`
 ADD CONSTRAINT `ticket_messages_ibfk_1` FOREIGN KEY (`ticket_id`) REFERENCES `tickets` (`id`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tokens`
+--
+ALTER TABLE `tokens`
+ADD CONSTRAINT `tokens_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE;
 SET FOREIGN_KEY_CHECKS=1;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
